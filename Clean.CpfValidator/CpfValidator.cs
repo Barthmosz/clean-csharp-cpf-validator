4e4436fb83d385dee74e6721a84f32ccf645d8e0
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-04 22:14:59 -0300

    feat: ensure CpfValidator validates properly

---
+++ Clean.CpfValidator/CpfValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Clean.CpfValidator
+{
+    public static class CpfValidator
+    {
+        public static bool Validate(string cpf)
+        {
+            string cpfRegex = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$";
+            return Regex.IsMatch(cpf, cpfRegex);
+        }
+    }
+}
