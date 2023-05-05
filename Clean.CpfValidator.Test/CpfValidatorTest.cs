1cb5dafa6cbd927334b73ef3382422e5f3b18ace
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-04 22:15:15 -0300

    test: ensure CpfValidator returns false if an invalid cpf is provided

---
+++ Clean.CpfValidator.Test/CpfValidatorTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Clean.CpfValidator.Test
+{
+    [TestFixture]
+    public class CpfValidatorTest
+    {
+        [Test, Description("ensure CpfValidator returns false if an invalid cpf is provided")]
+        public void CpfValidator_InvalidCpf_ReturnsFalse()
+        {
+            Assert.That(CpfValidator.Validate("123.456.678"), Is.EqualTo(false));
+        }
+    }
+}
